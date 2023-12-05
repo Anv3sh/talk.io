855d5f45a3bc27c2f80ebb479bb1c6dedfe60642
briheet01 <briheetyadav@gmail.com>
2023-12-05 18:53:02 +0530

    Made changes by adding Message to Group model

--- src/backend/talk/database/models/messages.py
+++ src/backend/talk/database/models/messages.py
@@ -1,38 +1,28 @@
 from uuid import UUID, uuid4
-from typing import Optional, Dict, TYPE_CHECKING
+from typing import Optional, Dict, TYPE_CHECKING, List
 from sqlmodel import SQLModel, Field, Relationship
 from datetime import datetime, timezone
 import sqlalchemy as sa
-from pydantic import EmailStr
+from pydantic import EmailStr, BaseModel, Field
+
+from talk.database.models.base import SQLModelSerializable
 
 
 if TYPE_CHECKING:
-    from talk.database.models.pfp import pfp
+    from talk.database.models.pfp import PFP
 
 
-class Message(SQLModelSerializable, table=True):
-    message_id: UUID = Field(default_factory=uuid4, primary_key=True, unique=True)
-    sender_id: UUID = Field(foreign_key="sender.sender_id")
-    receiver_id: UUID = Field(foreign_key="receiver.receiver_id")
-    content: str
-    time: Optional[datetime] = Field(
-        sa_column=sa.Column(sa.DateTime(timezone=True), nullable=False),
-        default_factory=lambda: datetime.utcnow().replace(tzinfo=timezone.utc),
-    )
-    message_recipient: Optional[bool] = Field(default=False)
-    content_type: Optional[str] = Field(default="text")
+class Message(SQLModelSerializable):
+    sender_id: UUID = Field(default_factory=uuid4, primary_key=True)
+    message: Optional[str] = Field(default="")
+    content_type: Optional[str] = Field(default="")
+    content: Optional[bytes] = Field(default=None)
+    group_id: UUID = Field(sa.ForeignKey("group.id"))
+    group: Group = Relationship(back_populates="messages")
 
-    sender: Optional['Sender'] = Relationship(foreign_key=(sender_id, "Sender.sender_id"))
-    receiver: Optional['Receiver'] = Relationship(foreign_key=(receiver_id, "Receiver.receiver_id"))
+class Group(SQLModel):
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str
+    messages: List[Message] = Relationship(back_populates="group")
 
 
-class Sender(SQLModelSerializable, table=True):
-    sender_id: UUID = Field(default_factory=uuid4, primary_key=True, unique=True)
-    email: EmailStr
-    messages: List[Message] = Relationship(back_populates="sender")
-
-
-class Receiver(SQLModelSerializable, table=True):
-    receiver_id: UUID = Field(default_factory=uuid4, primary_key=True, unique=True)
-    email: EmailStr
-    received_messages: List[Message] = Relationship(back_populates="receiver")
